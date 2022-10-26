5cfc10d99722275cf4782eb6782237e0c6ae7f08
Silva Filho <tarusilva@gmail.com>
2022-10-26 17:55:58 -0300

    feat: cria conexão com o banco de dados.
    Cria o pool de conexão com o banco de dados usando
    variáveis de ambiente.

---
+++ src/database/conexao.js
@@ -0,0 +1,17 @@
+const { Pool } = require( "pg" );
+
+const pool = new Pool( {
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PWD,
+    port: Number( process.env.DB_PORT )
+} );
+
+const query = ( text, params ) => {
+    return pool.query( text, params );
+};
+
+module.exports = {
+    query
+};
